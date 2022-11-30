3539ddd2eb9cf1c2a02ea1f0658ea613b99b7147
WesleySchorel <wesleyschorelhva@gmail.com>
2022-11-30 16:22:12 +0100

    Navbar update

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,18 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
