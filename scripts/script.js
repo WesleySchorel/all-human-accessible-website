36ac6c09788047de400a9a61f5f0a22339a7e62f
WesleySchorel <wesleyschorelhva@gmail.com>
2022-12-22 13:11:04 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 /* DARK MODE */
-function myFunction() {
-    var element = document.body;
+function toggleDarkMode() {
+    let element = document.body;
     element.classList.toggle("dark-mode");
  }
 
  /* ACCORDION */
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
@@ -21,8 +21,8 @@ for (i = 0; i < acc.length; i++) {
 }
 
 /* FEEDBACK BUTTON */
-function myFeedback() {
-  var x = document.getElementById("feedback-button");
+function checkFeedbackButtonContent() {
+  let x = document.getElementById("feedback-button");
   if (x.innerHTML === "Bedankt voor uw feedback.") {
       x.innerHTML = "Vond u deze pagina nuttig?";
   } else {
@@ -33,14 +33,14 @@ function myFeedback() {
 /* RICHTLIJNEN DIALOG */
 
 let button = document.getElementById('selecteer-richtlijn-button')
-var dialogRichtlijnen = document.querySelector('#selecteer-richtlijn-dialog')
+let dialogRichtlijnen = document.querySelector('#selecteer-richtlijn-dialog')
 
 button.addEventListener('click', function() {
   dialogRichtlijnen.showModal()
 })
 
 dialogRichtlijnen.addEventListener('submit', function() {
-  var selectThingy = document.querySelector('select')
+  let selectThingy = document.querySelector('select')
   location.href = selectThingy.value
 })
 
